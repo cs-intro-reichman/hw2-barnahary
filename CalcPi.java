82f37de4d436f5ff5a25d9de32b9e0e500169ebe
barnahary <bar.nahary@post.runi.ac.il>
2024-11-19 11:22:46 +0200

    done

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,18 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int n = Integer.parseInt(args[0]);
+		int one = 1;
+		double zero = 0.0;
+		for (int i = 0; i < n; i++) {
+			int b = 2 * i + 1;
+			zero += one * (1.0 / b);
+			one = -one;
+		}
+		zero *= 4;
+		double pie = Math.PI;
+		System.out.println("pi according to java: " + pie);
+		System.out.println("pi, approximated: " + zero);
+		
 	}
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,30 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int N = Integer.parseInt(args[0]);
+		
+		for (int i = 1; i <= N; i++) {
+			int steps = 1;
+			int t = i;
+			if (i == 1) {
+				System.out.print(" 1 4 2 1");
+				System.out.println(" (4)");
+				continue;
+			}
+			System.out.print(" " + t);
+			
+			while ( t != 1 ) {
+		if (t % 2 == 0) { 
+			t = t / 2;
+		} else {
+			t = (t *  3) + 1;
+		}
+		System.out.print(" " + t);
+		steps++;
 	}
-}
+	System.out.println( " ("+ steps +")");
+		}	
+	}
+	}
+
+
